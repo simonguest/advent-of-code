ed051305e26473b670a68941c283eef4d376b570
Simon Guest <simon@code.org>
2021-12-09 16:11:35 -0800

    Day 8 Lesson 1 fun

--- 2021/09/smoke_test.rb
+++ 2021/09/smoke_test.rb
@@ -6,9 +6,9 @@ require_relative './smoke'
 # Tests for Day Eight
 class DayNineTests < MiniTest::Test
   def test_smoke_part_one
-    # total = Smoke.scan_map './09/testdata.txt'
-    # p total
-    # assert total == 15
-    p Smoke.scan_map './09/data.txt'
+    total = Smoke.scan_map_for_low_points './09/testdata.txt'
+    p total
+    assert total == 15
+    p Smoke.scan_map_for_low_points './09/data.txt'
   end
 end
